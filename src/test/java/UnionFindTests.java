e2aceaff6a3dddd1fa97794853b6631323674496
Yasin <117643098+YasinBou@users.noreply.github.com>
2025-06-18 01:17:16 +0200

    Fix folder structure and add test class

---
+++ src/test/java/UnionFindTests.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UnionFindTests {
+
+    @Test
+    public void test() {
+        assertEquals(4, 4);
+    }
+}
